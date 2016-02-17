700ab89c6f26a825e890943ac499ab58aab397a7
Thiago Andrade <thiagoh@gmail.com>
2016-02-17 08:53:01 -0300

    logo image

--- core/src/com/tapiocagames/game/StartScreen.java
+++ core/src/com/tapiocagames/game/StartScreen.java
@@ -26,6 +26,7 @@ public class StartScreen extends ScreenAdapter {
 
     private Stage stage;
     private Texture bgTexture;
+    private Texture logoTexture;
     private Texture playUpTexture;
     private Texture playDownTexture;
 
@@ -40,6 +41,7 @@ public class StartScreen extends ScreenAdapter {
         Gdx.input.setInputProcessor(stage);
 
         bgTexture = new Texture(Gdx.files.internal("bg.png"));
+        logoTexture = new Texture(Gdx.files.internal("start-screen-logo.png"));
         playDownTexture = new Texture(Gdx.files.internal("play-down.png"));
         playUpTexture = new Texture(Gdx.files.internal("play-up.png"));
 
@@ -57,7 +59,12 @@ public class StartScreen extends ScreenAdapter {
         });
 
         Image bgImage = new Image(bgTexture);
+        Image logoImage = new Image(logoTexture);
+
+        logoImage.setPosition(WORLD_WIDTH / 2, WORLD_HEIGHT * 3.f / 4.f, Align.center);
+
         stage.addActor(bgImage);
+        stage.addActor(logoImage);
         stage.addActor(playButton);
     }
 
